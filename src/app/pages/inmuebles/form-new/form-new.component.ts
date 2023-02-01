dbc2adc4e414f9123f436c1ace7930386a0da41d
Ivan Jimenez <chiklete@gmail.com>
2023-02-01 17:42:05 +0100

    second commit

---
+++ src/app/pages/inmuebles/form-new/form-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-new',
+  templateUrl: './form-new.component.html',
+  styleUrls: ['./form-new.component.scss']
+})
+export class FormNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/inmuebles/inmuebles.component.ts
+++ src/app/pages/inmuebles/inmuebles.component.ts
@@ -3,25 +3,29 @@ import { Component, OnInit,OnDestroy  } from '@angular/core';
 import { ServicesService } from '../services.service';
 import { Subject, takeUntil } from 'rxjs';
 
+import { DialogService } from 'primeng/dynamicdialog';
+import { FormNewComponent } from './form-new/form-new.component';
 
 
 
 @Component({
   selector: 'app-inmuebles',
   templateUrl: './inmuebles.component.html',
-  styleUrls: ['./inmuebles.component.scss']
+  styleUrls: ['./inmuebles.component.scss'],
+  providers: [DialogService]
 })
 export class InmueblesComponent implements OnInit {
 
   //Aquí almacenaremos nuestro listado de pisos una vez se complete la petición
-  public pisosList : Models[] = []
+  // public pisosList : Models[] = []
+  public pisosList : any = []
   public indexPiso: number | null = null;
   public isOpen: boolean = false;
 
   protected readonly clearSubscriptions$ = new Subject();
 
 
-  constructor(public servicesService:ServicesService) { }
+  constructor(public servicesService:ServicesService, public dialogService:DialogService, ) { }
 
   ngOnInit(): void {
     this.recoverPisos(); 
@@ -33,6 +37,19 @@ export class InmueblesComponent implements OnInit {
     this.clearSubscriptions$.complete();
   }
 
+  newInmo(){
+      const ref = this.dialogService.open(FormNewComponent,
+        {
+          header: 'Nuevo inmueble',
+          width: '40%',
+          data:{
+            hola:'hola'
+          }
+  
+        })
+    } 
+  
+  
   recoverPisos() {
     return this.servicesService.getInmueble().pipe(takeUntil(this.clearSubscriptions$),).subscribe((data)=> {
       console.log(data)
